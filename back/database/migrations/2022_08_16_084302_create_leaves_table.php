3149a04c7720910cd41c75de8da43c889409e935
SOKUNBOPHA.SOVANN <SOKUNBOPHA.SOVANN@pnc.lan>
2022-08-16 16:28:57 +0700

    #15 create database table

---
+++ back/database/migrations/2022_08_16_084302_create_leaves_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('leaves', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('student_id');
+            $table->foreign('student_id')->references('id')->on('students')->onDelete('cascade');
+            $table->string('start_date');
+            $table->string('end_date');
+            $table->integer('duration');
+            $table->string('reason');
+            $table->string('leave_type');
+            $table->string('direction');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('leaves');
+    }
+};
