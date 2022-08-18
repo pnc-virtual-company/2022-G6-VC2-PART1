dee0fd3f11b8bfb3e07c5ba9e09fb33f7a89a805
VICHET.MORM <VICHET.MORM@pnc.lan>
2022-08-18 11:16:42 +0700

    #16 crud student

--- back/app/Models/Student.php
+++ back/app/Models/Student.php
@@ -4,8 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class Student extends Model
 {
-    use HasFactory;
+    use HasApiTokens, HasFactory, Notifiable;
+    protected $fillable=['name','class_room','gender','email','password','remember_token'];
 }
